f7fbd0790e5c08b0db2495a5b94ac57ce7e693a3
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-05-02 18:43:00 -0300

    chore(db): update initial user migration

--- prisma/migrations/20260502213417_add_user_model/migration.sql
+++ prisma/migrations/20260502213417_add_user_model/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Example` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Example";
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" UUID NOT NULL,
